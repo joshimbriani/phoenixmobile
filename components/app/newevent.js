6b8e75f1563adfe9431a99db0fb7de6f20c0f163
Josh Imbriani <joshimbriani@gmail.com>
2018-02-07 08:31:32 -0600

    Started work on New Event page

--- components/app/newevent.js
+++ components/app/newevent.js
@@ -8,6 +8,7 @@ import * as colorActions from '../../redux/actions/backgroundColor'
 import ColorScheme from 'color-scheme';
 import Swiper from 'react-native-swiper';
 import DateTimePicker from 'react-native-modal-datetime-picker';
+import fontBasedOnPlatform from '../utils/fontBasedOnPlatform';
 
 const ITEMS_TO_VALIDATE = ["title", "description", "place", "datetime"];
 
@@ -88,37 +89,48 @@ class NewEvent extends React.Component {
     render() {
         return (
             <Swiper nextButton={<Text>&gt;</Text>} prevButton={<Text>&lt;</Text>} style={styles.wrapper} showsButtons={true} loop={false} removeClippedSubviews={false} >
-                <View>
-                    <View style={[styles.header, { backgroundColor: "red" }]}>
-                        <Text style={styles.h1}>What?</Text>
-                        <Text style={styles.h2}>Give us a short and a longer description of what you want to do!</Text>
+                <View style={styles.flex1}>
+                    <View style={styles.header}>
+                        <Text style={styles.questionHeader}>What?</Text>
+                        <View style={styles.tagline}>
+                            <Text style={styles.taglineText}>Give us a short and a longer</Text>
+                            <Text style={styles.taglineText}>description of what you want to do!</Text>
+                        </View>
                     </View>
-                    <Form>
-                        <Item floatingLabel>
-                            <Label>Title</Label>
-                            <Input name="title" onChangeText={(text) => this.onChange("title", text)} />
-                        </Item>
-                        <Item floatingLabel last>
-                            <Label>Full Description</Label>
-                            <Input style={{
-                                width: 200, height: 500
-                            }} name="description" multiline={true} onChangeText={(text) => this.onChange("description", text)} />
-                        </Item>
-                    </Form>
+                    <View style={styles.formContainer}>
+                        <Content style={styles.flex1}>
+                            <Form>
+                                <Item stackedLabel>
+                                    <Label>Title</Label>
+                                    <Input name="title" onChangeText={(text) => this.onChange("title", text)} />
+                                </Item>
+                                <Item stackedLabel last>
+                                    <Label>Full Description</Label>
+                                    <Input style={{ height: 330, width: 300 }} name="description" multiline={true} onChangeText={(text) => this.onChange("description", text)} />
+                                </Item>
+                            </Form>
+                        </Content>
+                    </View>
                 </View>
-                <View>
-                    <View style={[styles.header, { backgroundColor: "red" }]}>
-                        <Text style={styles.h1}>Standing Offers?</Text>
-                        <Text style={styles.h2}>Any of these standing offers from businesses apply? You could save money if you choose one!</Text>
+                <View style={styles.flex1}>
+                    <View style={styles.header}>
+                        <Text style={styles.questionHeader}>Offers?</Text>
+                        <View style={styles.tagline}>
+                            <Text style={styles.taglineText}>Do any of these standing</Text>
+                            <Text style={styles.taglineText}>offers apply to your event?</Text>
+                        </View>
                     </View>
                     <View>
                         <Text>Sub List for standing offers</Text>
                     </View>
                 </View>
-                <View>
-                    <View style={[styles.header, { backgroundColor: "red" }]}>
-                        <Text style={styles.h1}>Where?</Text>
-                        <Text style={styles.h2}>Where is your event at?</Text>
+                <View style={styles.flex1}>
+                    <View style={styles.header}>
+                        <Text style={styles.questionHeader}>Where?</Text>
+                        <View style={styles.tagline}>
+                            <Text style={styles.taglineText}>Where is</Text>
+                            <Text style={styles.taglineText}>your event at?</Text>
+                        </View>
                     </View>
                     <Form>
                         <Item floatingLabel>
@@ -127,10 +139,13 @@ class NewEvent extends React.Component {
                         </Item>
                     </Form>
                 </View>
-                <View>
-                    <View style={[styles.header, { backgroundColor: "red" }]}>
-                        <Text style={styles.h1}>When?</Text>
-                        <Text style={styles.h2}>When do you wanna have your event?</Text>
+                <View style={styles.flex1}>
+                    <View style={styles.header}>
+                        <Text style={styles.questionHeader}>When?</Text>
+                        <View style={styles.tagline}>
+                            <Text style={styles.taglineText}>When do you want</Text>
+                            <Text style={styles.taglineText}>to have your event?</Text>
+                        </View>
                     </View>
                     <Form>
                         <Button title="SetDateTime" accessibilityLabel="Press this button to set the date and time of your event!" onPress={this._showDateTimePicker}>
@@ -145,9 +160,12 @@ class NewEvent extends React.Component {
                     />
                 </View>
                 <View>
-                    <View style={[styles.header, { backgroundColor: "red" }]}>
-                        <Text style={styles.h1}>Who?</Text>
-                        <Text style={styles.h2}>How many people are you looking to do your event with?</Text>
+                    <View style={styles.header}>
+                        <Text style={styles.questionHeader}>Who?</Text>
+                        <View style={styles.tagline}>
+                            <Text style={styles.taglineText}>How many people are</Text>
+                            <Text style={styles.taglineText}>you looking to join you?</Text>
+                        </View>
                     </View>
                     <Form>
                         <Item floatingLabel>
@@ -193,13 +211,8 @@ const styles = StyleSheet.create({
         alignSelf: 'stretch',
         height: 200,
     },
-    wrapper: {
-    },
-    slide1: {
+    flex1: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#9DD6EB',
     },
     slide2: {
         flex: 1,
@@ -219,6 +232,29 @@ const styles = StyleSheet.create({
         paddingTop: 5,
         textAlign: 'center',
         fontFamily: 'Roboto_medium'
+    },
+    header: {
+        flexDirection: 'row'
+    },
+    questionHeader: {
+        fontFamily: fontBasedOnPlatform(),
+        fontSize: 40,
+        marginTop: 10,
+        marginLeft: 10
+    },
+    tagline: {
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        flex: 1,
+        marginTop: 15,
+        marginRight: 20
+    },
+    taglineText: {
+        fontSize: 10,
+        fontWeight: '300'
+    },
+    formContainer: {
+        flex: 1,
     }
 
 });
